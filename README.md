e81d53d0bdf4478a4660b7f2b60ec56d35077976
corb1999 <le.corbusier.29845@gmail.com>
2021-09-22 21:19:36 -0400

    Created starter kits folder and built the first generic starter script for template usage when starting any kind of new project.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # cheatcraft
 A potluck of R code snippets and small helper functions.
+
+What is in here?
+- Starter Kits: templates for analysis scripts and other boilerplate starters
+
+
---
+++ starter_kits/starter_generic_script.R
@@ -0,0 +1,91 @@
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+# INTRO ====================================================================
+metadatar <- list(script_starttime = Sys.time(), 
+                  script_det = list(version_dt = as.Date("1999-01-01"), 
+                                    author = "", 
+                                    proj_name = "", 
+                                    script_type = "", 
+                                    notepad = paste0("")), 
+                  seed_set = 6)
+metadatar
+
+# LOAD LIBRARIES **********************************************************
+R.version.string
+Sys.info()
+getwd()
+library(lobstr)
+library(rlang)
+library(tidyverse)
+library(tidylog)
+library(lubridate)
+library(scales)
+library(gt)
+set.seed(metadatar$seed_set[1])
+options(digits = 4, max.print = 99, warnPartialMatchDollar = TRUE, 
+        tibble.print_max = 30, scipen = 999, nwarnings = 5)
+mem_used()
+
+# basic helper functions ***************************************************
+
+# function to print object size
+sizer <- function(x) {
+  aaa <- format(object.size(x), "MB")
+  return(aaa)}
+
+# function to quickly run garbage collection
+trash <- function(x) {
+  gc(verbose = TRUE)}
+
+# function to quickly view a sample of a dataframe
+viewer <- function(x) {
+  if (is.data.frame(x) == FALSE) {
+    print("Error, insert a dataframe")
+  } else {
+    if(nrow(x) < 95) {
+      View(x[sample(1:nrow(x), floor(nrow(x) * 0.5)), ])
+    } else {
+      View(x[sample(1:nrow(x), 100), ])
+    }}}
+
+# a function to make a quick data dictionary of a data frame
+data_dictionary <- function(aa) {
+  dd <- data.frame(column_order = seq(1, ncol(aa)), 
+                   column_name_text = colnames(aa), 
+                   column_class = sapply(aa, class, simplify = TRUE), 
+                   column_nacount = sapply(lapply(aa, is.na), 
+                                           sum, simplify = TRUE), 
+                   column_uniques = sapply(lapply(aa, unique), 
+                                           length, simplify = TRUE), 
+                   row_01 = sapply(aa[1, ], as.character, simplify = TRUE), 
+                   row_02 = sapply(aa[2, ], as.character, simplify = TRUE),
+                   row_03 = sapply(aa[3, ], as.character, simplify = TRUE),
+                   row_04 = sapply(aa[4, ], as.character, simplify = TRUE),
+                   row_05 = sapply(aa[5, ], as.character, simplify = TRUE),
+                   row.names = NULL)
+  return(dd)}
+
+# start the clock timer, used for monitoring runtimes
+clockin <- function() {
+  aa <- Sys.time()
+  clock_timer_start <<- aa
+  return(aa)}
+
+# end the clock timer, used in conjunction with the clockin fun
+clockout <- function(x) {
+  aa <- clock_timer_start
+  bb <- Sys.time()
+  cc <- bb - aa
+  return(cc)}
+
+# helps turn a character dollar variable into numeric
+#   requires stringr, uncomment last line to turn NA to zero
+cash_money <- function(x) {
+  aa <- str_remove_all(x, pattern = "\\$")
+  bb <- str_remove_all(aa, pattern = ",")
+  cc <- as.numeric(bb)
+  # cc <- ifelse(is.na(cc), 0, cc)
+  return(cc)}
+
+# ^ ====================================
+# :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
