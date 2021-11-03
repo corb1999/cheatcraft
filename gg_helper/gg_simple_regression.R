96d9acd01c315883237c01c4ed69a95068f3e2e0
corb1999 <le.corbusier.29845@gmail.com>
2021-11-02 20:56:26 -0400

    added patchwork library to allow additional visuals in the simple regression function. added plots of the variable distributions, and added plots showing the standardized residuals of the model.

--- gg_helper/gg_simple_regression.R
+++ gg_helper/gg_simple_regression.R
@@ -3,10 +3,11 @@
 
 library(tidyverse)
 library(rlang)
+library(patchwork)
 xx1 <- runif(100) * 10
 dfa <- data.frame(xx_a = xx1, 
                   yy_a = rbinom(100, 100, 0.1) + xx1, 
-                  ww_a = runif(100) + xx1 / 2, 
+                  ww_a = runif(100) + xx1 / 5, 
                   zz = sample(c(letters), size = 100, replace = TRUE))
 rm(xx1)
 
@@ -39,16 +40,14 @@ fun_scatter_regression <- function(dfz, xvar, yvar, weight_var = NA,
                     "; y = ", round(m1$coefficients[[2]], 3), "x + ",
                     round(m1$coefficients[[1]], 3))
   p_caption <- paste(pred_targets, collapse = "; ")
-  return_me <- dfz %>%
-    ggplot(aes(x = xx, y = yy)) + 
+  plt1 <- dfz %>% ggplot(aes(x = xx, y = yy)) + 
     geom_vline(aes(xintercept = median(xx)), 
                color = '#334756', linetype = 2) + 
     geom_hline(aes(yintercept = median(yy)), 
                color = '#334756', linetype = 2) + 
     geom_point(aes(x = median(xx), y = median(yy)), 
                color = '#334756', size = 2.5, shape = 1) + 
-    geom_rug(alpha = 0.25) +
-    geom_point(alpha = 0.85) +
+    geom_rug(alpha = 0.25) + geom_point(alpha = 0.85) +
     geom_abline(slope = m1$coefficients[[2]],
                 intercept = m1$coefficients[[1]],
                 size = 1, linetype = 2, color = "blue") +
@@ -63,6 +62,29 @@ fun_scatter_regression <- function(dfz, xvar, yvar, weight_var = NA,
     labs(subtitle = plt_sub, x = xvar, y = yvar, 
          caption = paste("Prediction x-values = ", p_caption, "\n", 
                          "Weights = ", weight_var))
+  plt1x <- dfz %>% ggplot(aes(x = xx)) + 
+    geom_histogram(bins = 30, color = 'white', alpha = 0.5) + 
+    geom_vline(aes(xintercept = median(xx))) + 
+    theme_minimal() + labs(x = xvar, y = '')
+  plt1y <- dfz %>% ggplot(aes(x = yy)) + 
+    geom_histogram(bins = 30, color = 'white', alpha = 0.5) + 
+    geom_vline(aes(xintercept = median(yy))) + 
+    theme_minimal() + labs(x = yvar, y = '')
+  rr_std <- (residuals(m1) - mean(residuals(m1))) / 
+    sd(residuals(m1))
+  plt2 <- dfz %>% ggplot(aes(x = rr_std)) + 
+    geom_histogram(bins = 30, color = 'white', 
+                   fill = 'blue', alpha = 0.5) + 
+    geom_vline(aes(xintercept = 0)) + theme_minimal() + 
+    labs(x = 'Standardized Residuals', y = 'Freq')
+  plt3 <- ggplot() + 
+    geom_point(data = dfz, aes(x = xx, y = rr_std, 
+                               color = rr_std)) + 
+    geom_hline(aes(yintercept = 0)) + 
+    scale_color_distiller(palette = 'RdYlBu') + 
+    theme_minimal() + theme(legend.position = 'none') + 
+    labs(x = xvar, y = 'Standardized Residuals')
+  return_me <- plt1 + (plt1x / plt1y) + (plt2 / plt3)
   return(return_me)}
 
 # tests ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -71,6 +93,16 @@ fun_scatter_regression(dfa, xvar = 'xx_a', yvar = 'yy_a',
                        pred_targets = c(2, 5, 8))
 fun_scatter_regression(dfa, xvar = 'xx_a', yvar = 'yy_a', 
                        weight_var = 'ww_a')
+
+fun_scatter_regression(longley, 'Employed', 'GNP')
+fun_scatter_regression(swiss, 'Agriculture', 'Fertility')
+fun_scatter_regression(trees, 'Girth', 'Height')
+fun_scatter_regression(women, 'height', 'weight')
+
+fun_scatter_regression(iris, 'Sepal.Length', 'Sepal.Width')
+fun_scatter_regression(USJudgeRatings, 'CONT', 'INTG')
+fun_scatter_regression(airquality, 'Temp', 'Wind')
+fun_scatter_regression(faithful, 'waiting', 'eruptions')
 
 # ^ -----
 
