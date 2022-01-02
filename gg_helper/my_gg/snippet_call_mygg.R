695c0073bbbd2b3d8cbbb1402902ccb4a637e9e8
corb1999 <le.corbusier.29845@gmail.com>
2022-01-02 15:34:43 -0500

    a snippet to pull down the customized ggplot theme function/arguments. Basically if you source the script from the github repo you can pull the function in the current environment

--- gg_helper/my_gg/snippet_call_mygg.R
+++ gg_helper/my_gg/snippet_call_mygg.R
@@ -1,6 +1,7 @@
 
-
+# library(tidyverse)
 # snippet to call the function from the github repo
 
-
-
+source(file = paste0('https://raw.githubusercontent.com/', 
+                     'corb1999/cheatcraft/', 
+                     'main/gg_helper/my_gg/my_gg_settings.R'))
