0fed3b65c0dad69dfb3e837a54b2830c0848b0d2
corb1999 <le.corbusier.29845@gmail.com>
2022-01-26 20:02:22 -0500

    had to remove the guide colorbar code snippets because it will throw an error on discrete data, which is what the second test is about. will have to add that arg custom instead of including it in the theme itself. also added some additional args to the theme based on snippets from ggplot tutorial site

--- gg_helper/my_gg/my_gg_settings.R
+++ gg_helper/my_gg/my_gg_settings.R
@@ -8,25 +8,32 @@ my_gg <- function(feedplot) {
           plot.title.position = 'plot', 
           plot.title = element_text(size = 12, 
                                     face = 'bold'), 
-          plot.subtitle = element_text(size = 10)) + 
-    guides(fill = guide_colorbar(title.position = 'left', 
-                                 title.hjust = 0.5, 
-                                 barwidth = unit(20, 'lines'), 
-                                 barheight = unit(0.5, 'lines')), 
-           color = guide_colorbar(title.position = 'left', 
-                                  title.hjust = 0.5, 
-                                  barwidth = unit(20, 'lines'), 
-                                  barheight = unit(0.5, 'lines'))) + 
+          plot.subtitle = element_text(size = 10), 
+          axis.title.x = element_text(size = 10), 
+          axis.title.y = element_text(size = 10), 
+          plot.caption.position = 'plot', 
+          plot.caption = element_text(size = 8, 
+                                      face = 'italic'), 
+          panel.grid.minor = element_blank()) + 
+    guides(color = guide_legend(direction = 'horizontal')) + 
     coord_cartesian(clip = 'off')
   return(return_me)}
 
 
 # tests ??????????????????????????????????????????????
 
-# (plt1 <- mtcars %>% 
-#   ggplot(aes(x = mpg, y = qsec, color = disp)) + 
-#   geom_point() + 
-#   labs(title = 'asdf', subtitle = 'qwerty', 
+# (plt1 <- mtcars %>%
+#   ggplot(aes(x = mpg, y = qsec, color = disp)) +
+#   geom_point() +
+#   labs(title = 'asdf', subtitle = 'qwerty',
 #        caption = 'hello'))
 # 
 # my_gg(plt1)
+# 
+# (plt2 <- mtcars %>%
+#     ggplot(aes(x = mpg, y = disp, color = as.factor(cyl))) +
+#     geom_point() +
+#     labs(title = 'asdf', subtitle = 'qwerty',
+#          caption = 'hello'))
+# 
+# my_gg(plt2)
